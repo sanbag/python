e94351ff1e7b0663607f33d2e204b576e60539d3
SantoshBaggon <114639857+SantoshBaggon@users.noreply.github.com>
2024-10-30 23:37:06 +0530

    new era

--- DSA/a_sorting/selection.py
+++ DSA/a_sorting/selection.py
@@ -1,1 +1,13 @@
 
+def selection_sort(arr):
+
+    for i in range(len(arr)):
+        min =i
+        for j in range(i+1,len(arr)):
+            if arr[j]<arr[min]:
+                min = j
+        arr[i],arr[min]=arr[min],arr[i]
+
+    return arr
+
+print(selection_sort([64, 25, 12, 22, 11]))

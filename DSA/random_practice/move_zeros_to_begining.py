227446ebc7654ae9be6636c823a1f205ccff2443
SantoshBaggon <114639857+SantoshBaggon@users.noreply.github.com>
2024-10-31 20:49:22 +0530

    31-10-2024

---
+++ DSA/random_practice/move_zeros_to_begining.py
@@ -0,0 +1,10 @@
+def move_zeros_to_begining(arr):
+    pointer = 0
+    for i in range(len(arr)):
+        if arr[i]==0:
+            arr[pointer],arr[i]=arr[i],arr[pointer]
+            pointer+=1
+
+    return arr
+
+print(move_zeros_to_begining([1,0,0,1,3,4]))

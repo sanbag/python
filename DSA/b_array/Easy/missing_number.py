310c9365c586bff65225164abf6d897663a80d7b
SantoshBaggon <baggonsantosh@gmail.com>
2024-11-12 23:22:46 +0530

    2024-11-12

--- DSA/b_array/Easy/missing_number.py
+++ DSA/b_array/Easy/missing_number.py
@@ -7,3 +7,17 @@ def missing_number(arr,n):
     return total_sum-sum
 
 print(missing_number([1, 2, 4, 5],5))
+
+def xor_missing_number(arr,n):
+    xor1 =0
+    xor2 =0
+
+    for i in range(n+1):
+        xor1=xor1 ^i
+
+    for i in arr:
+        xor2 = xor2^i
+
+    return xor1^xor2
+
+print(xor_missing_number([1, 2, 4, 5],5))

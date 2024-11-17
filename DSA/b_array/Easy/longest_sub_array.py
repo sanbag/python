8207d5638a6d2cf1099366b92c23cd93c7d77708
Santosh Baggon <baggonsantosh@gmail.com>
2024-11-18 00:30:09 +0530

    2024-11-17

--- DSA/b_array/Easy/longest_sub_array.py
+++ DSA/b_array/Easy/longest_sub_array.py
@@ -16,3 +16,27 @@ def longest_sub_array_brute(arr,k):
 
 
 print(longest_sub_array_brute([1,2,3,1,1,1,1,4,2,3],4))
+
+def longest_sub_array_hash_map(arr,k):
+    sum =0
+    maxlength =0
+
+    dict={}
+
+    for i in range(len(arr)):
+        sum+=arr[i]
+        if sum == k:
+            maxlength = max(maxlength,i+1)
+
+        remain = sum-k
+
+        if sum not in dict:
+            dict[sum]  =i
+        print(dict)
+        if remain in dict:
+            length =  i - dict[remain]
+            maxlength = max(maxlength,length)
+
+    return maxlength
+
+print(longest_sub_array_hash_map([1,2,3,1,1,1,1,4,2,3],3))

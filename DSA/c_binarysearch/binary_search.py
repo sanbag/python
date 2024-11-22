c8f44a49043193c65e9ea559102a2d228295b2e5
Santosh Baggon <baggonsantosh@gmail.com>
2024-11-22 22:35:33 +0530

    2024-11-22

--- DSA/c_binarysearch/binary_search.py
+++ DSA/c_binarysearch/binary_search.py
@@ -1,1 +1,18 @@
-def binary_search(arr,key):
+
+
+def binary_search(arr,target):
+    low = 0
+    high =  len(arr)-1
+
+    while high>=low:
+        mid = (low+high)//2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid]<target:
+            low =mid+1
+        else:
+            high = mid-1
+    return  -1
+
+print(binary_search([1,2,3,4,5,6,7,8],8))

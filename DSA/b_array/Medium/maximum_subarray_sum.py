7ae6fe0bea3e8a62e332449cba5c11337397138d
Santosh Baggon <baggonsantosh@gmail.com>
2024-11-26 23:52:48 +0530

    2024-11-26

--- DSA/b_array/Medium/maximum_subarray_sum.py
+++ DSA/b_array/Medium/maximum_subarray_sum.py
@@ -2,18 +2,19 @@ def max_sub_array(arr):
     maximum_sum =  float('-inf')
     sum =0
     start_index =-1
+    temp_start_index =-1
     end_index = -1
     for i in range(len(arr)):
         if sum ==0:
-            start_index =i
+            temp_start_index =i
         sum = sum + arr[i]
 
         if sum>maximum_sum:
             maximum_sum = sum
-            # start_index = start_index
+            start_index = temp_start_index
             end_index = i
 
         if sum<0:
             sum = 0
     return  maximum_sum,start_index,end_index
-print(max_sub_array([-2,3,4,-1,-2,1,7,-3,-1,-1,-1]))
+print(max_sub_array([-2,3,4,-1,-2,1,7,-3,-1,-10,-1]))

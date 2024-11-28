993ff8f2b63578306206ed067694caed4cf12d6c
SantoshBaggon <baggonsantosh@gmail.com>
2024-11-28 23:25:36 +0530

    2024-11-28

--- DSA/practice/practice.py
+++ DSA/practice/practice.py
@@ -1,20 +1,22 @@
-def move_zero_to_end(arr):
-    l=len(arr)-1
-    for i  in range(len(arr)-1,-1,-1):
-        if arr[i]==0:
-            arr[l],arr[i]=arr[i],arr[l]
-            l-=1
-
-    return arr
-print(move_zero_to_end([1,0,0,1,1,0,1,1,1,0]))
-
-def move_zero(arr):
-    l=0
+def max_sub_array(arr):
+    maximum_sum = float("-inf")
+    sum =0
+    start_index = -1
+    end_index = -1
+    tem_start_index = -1
     for i in range(len(arr)):
-        if arr[i] !=0:
-            arr[i],arr[l]=arr[l],arr[i]
-            l+=1
-    return arr
-print(move_zero([1,0,0,1,1,0,1,1,1,0]))
+        if sum == 0:
+            tem_start_index = i
+        sum += arr[i]
 
-print('Hello')
+
+        if sum > maximum_sum:
+            maximum_sum =sum
+            start_index = tem_start_index
+            end_index = i
+
+        if sum <0:
+            sum = 0
+    return  maximum_sum,start_index,end_index
+
+print(max_sub_array([-2,-3,4,-1,-2,1,7,-3,-1,-10,-1]))

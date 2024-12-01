ac2bdc8cf12996bc0c5202f9af9b0398ebce965e
Santosh Baggon <baggonsantosh@gmail.com>
2024-12-01 23:31:19 +0530

    2024-12-01

--- DSA/b_array/Medium/rearrange_the_element_by_sign.py
+++ DSA/b_array/Medium/rearrange_the_element_by_sign.py
@@ -16,3 +16,29 @@ def rearrange_the_element_by_array(arr):
     return rearrange_array
 
 print(rearrange_the_element_by_array([3,1,-2,4,-5,-6]))
+
+def rearrange_the_element_by_array_second_type(arr):
+    possitive =[]
+    negative =[]
+
+    for i in arr:
+        if i>0:
+            possitive.append(i)
+        else:
+            negative.append(i)
+
+    equal_length = min(len(possitive),len(negative))
+
+    sorted =[]
+    for i in range(equal_length):
+        sorted.append(possitive[i])
+        sorted.append(negative[i])
+
+    if len(possitive)>equal_length:
+        sorted.extend(possitive[equal_length:])
+    else:
+        sorted.extend(negative[equal_length:])
+
+    return sorted
+
+print(rearrange_the_element_by_array_second_type([3,2,1,-2,-4,-5,-6]))

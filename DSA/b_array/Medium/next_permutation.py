4e27e2c0393a9d0ce10de037d059ca4a842d40b9
Santosh Baggon <baggonsantosh@gmail.com>
2024-12-04 00:25:12 +0530

    2024-12-03

--- DSA/b_array/Medium/next_permutation.py
+++ DSA/b_array/Medium/next_permutation.py
@@ -1,1 +1,27 @@
-print('hello')
+from audioop import reverse
+from operator import index
+
+
+def next_permutation(arr):
+    n = len(arr)
+    index = -1
+    for i in range(n-2,-1,-1):
+        if arr[i]<arr[i+1]:
+            index = i
+            break
+
+    if index == -1:
+        arr.reverse()
+        return arr
+
+    for i in range(n-1,index,-1):
+        if arr[i]> arr[index]:
+            arr[i],arr[index]=arr[index],arr[i]
+            break
+
+    arr[index+1:]= reversed(arr[index+1:])
+
+    return arr
+
+print(next_permutation([1, 2, 3, 6, 5, 4]))
+

4379395b18849b397279ecf7424ec7f658dddd91
Santosh Baggon <baggonsantosh@gmail.com>
2024-12-06 23:53:52 +0530

    2024-12-06

--- DSA/practice/practice.py
+++ DSA/practice/practice.py
@@ -1,21 +1,19 @@
-def buy_sell_stock(arr):
-    min_value = arr[0]
-    min_index = 0  # To track the index of the minimum value
-    max_diff = float("-inf")
-    start_day = -1
-    end_day = -1
+def binary_insert (arr,k):
+        l =0
+        r =len(arr)-1
 
-    for i in range(1, len(arr)):
-        diff = arr[i] - min_value
-        if diff > max_diff:
-            max_diff = diff
-            start_day = min_index  # Start day is where the min_value occurred
-            end_day = i
-        if arr[i] < min_value:
-            min_value = arr[i]
-            min_index = i  # Update the index of the new minimum value
+        while  l<r:
+            mid = (l+r)//2
 
-    return max_diff, start_day, end_day
+            if arr[mid] ==k:
+                return mid
 
+            elif arr[mid]>k:
+                r = mid-1
 
-print(buy_sell_stock([7, 2, 5, 6, 3, 6, 4,1,9]))
+            else:
+                l = mid+1
+
+        return l
+
+print(binary_insert([1,2,4,7],1))
